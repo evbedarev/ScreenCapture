f4fd50eec4d627178649de65211ac2c648b9ba17
mj <madjo85@yandex.ru>
2018-08-23 12:26:39 +0300

    change lootarround, add checkHp refactoring lootArround and kill monsters change attack change locationcheck

--- src/main/java/checks/location/Location.java
+++ src/main/java/checks/location/Location.java
@@ -12,23 +12,29 @@ public abstract class Location implements VerifyMap {
     protected static int[] PORTAL_RGB;
     protected static int[] PORTAL_RGB_1;
 
+
+    /**
+     * Проверяет находится ли RGB пикселя в нужном промежутке значений.
+     * PORTAL_RGB - array {x,y, minimal value RGB, max value RGB}
+     * @return boolean
+     */
     boolean checkRgbLocation() {
-        boolean findRGB;
+        boolean findRGB1, findRGB2;
         BufferedImage image = capture.takeScreenShot();
         int rgb = image.getRGB(PORTAL_RGB[0], PORTAL_RGB[1]);
         int rgb1 = image.getRGB(PORTAL_RGB_1[0], PORTAL_RGB_1[1]);
-        findRGB = (rgb > PORTAL_RGB[2] && rgb < PORTAL_RGB[3]) ||
-                (rgb1 > PORTAL_RGB[2] && rgb1 < PORTAL_RGB[3]);
-        if (findRGB) {
-            return findRGB;
+        findRGB1 = rgb > PORTAL_RGB[2] && rgb < PORTAL_RGB[3];
+        findRGB2 = rgb1 > PORTAL_RGB[2] && rgb1 < PORTAL_RGB[3];
+        if (findRGB1 || findRGB2) {
+            return true;
         } else {
             showRGB(PORTAL_RGB, image);
             showRGB(PORTAL_RGB_1, image);
-            return findRGB;
+            return false;
         }
     }
 
-    protected void showRGB(int[] xy, BufferedImage bufferedImage) {
+    private void showRGB(int[] xy, BufferedImage bufferedImage) {
         LoggerSingle.logInfo(this.toString(), "For coords {"+ xy[0] + "," + xy[1]+"} = "
                 + bufferedImage.getRGB(xy[0], xy[1]));
     }
