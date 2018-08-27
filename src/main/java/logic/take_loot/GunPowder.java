22f43b4f568391fc1a39f54fdebddcda2771e65a
mj <madjo85@yandex.ru>
2018-08-27 17:35:48 +0300

    change lootarround, add checkHp refactoring lootArround and kill monsters change attack

---
+++ src/main/java/logic/take_loot/GunPowder.java
@@ -0,0 +1,14 @@
+package logic.take_loot;
+
+import logic.RgbParameter;
+
+import java.awt.*;
+
+public class GunPowder extends Loot {
+    public GunPowder() throws AWTException {
+        rgbParameterList.add(new RgbParameter(-11781268,
+                new int[] {35,35},
+                new int[] {-10729608}));
+
+    }
+}
---
+++ src/main/java/logic/take_loot/Steel.java
@@ -0,0 +1,14 @@
+package logic.take_loot;
+
+import logic.RgbParameter;
+
+import java.awt.*;
+
+public class Steel extends Loot {
+    public Steel() throws AWTException {
+        rgbParameterList.add(new RgbParameter(-4349783,
+                new int[] {35,35},
+                new int[] {-5000239, -4276522}));
+
+    }
+}
