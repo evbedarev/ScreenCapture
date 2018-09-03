61af0e19c808426206fbd119103582a9be304855
mj <madjo85@yandex.ru>
2018-09-03 15:18:42 +0300

    change lootarround, add checkHp refactoring lootArround and kill monsters change attack + take screenshot after death

---
+++ src/main/java/logic/LogicWizard/LogicLocationWizard.java
@@ -0,0 +1,108 @@
+package logic.LogicWizard;
+
+import actions.Actions;
+import actions.SleepTime;
+import checks.CheckAgressorIsNear;
+import checks.CheckSP;
+import checks.LocationCheck;
+import checks.afterDeath.AfterDeath;
+import checks.check_hp.CheckHP;
+import logger.LoggerSingle;
+import logic.Logic;
+import logic.attacks.Attack;
+import logic.kill_monster.KillMonster;
+import logic.take_loot.LootAround;
+import logic.take_loot.TakeLoot;
+import main.Prop;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public abstract class LogicLocationWizard extends Thread implements Logic {
+    static int countOfAttacks = 100;
+    static List<KillMonster> killMonsterList;
+    static TakeLoot[] loot;
+    static TakeLoot[] usefulLoot;
+    static AfterDeath checkDie = Prop.checkDie;
+    static final CheckHP checkHP = CheckHP.instance();
+    static final CheckSP checkSP = CheckSP.instance();
+    static final CheckAgressorIsNear checkAgressorIsNear = CheckAgressorIsNear.instance();
+    int count = 0;
+    static Attack attack;
+    static final AtomicInteger ATTACK_TIMER = new AtomicInteger(0);
+    static final AtomicInteger ATTACK_MOBS_BEHIND_WALLS = new AtomicInteger(0);
+    static Actions actions;
+    static LocationCheck locationCheck;
+    static LootAround lootAround = LootAround.getInstance();
+
+    public abstract void createThread() throws Exception;
+
+    public void run() {
+        try {
+            checkHP.initialize(true, Prop.checkHitPoints);
+            actions = Actions.instance();
+            actions.initialize(loot, usefulLoot);
+            checkSP.initialize();
+            while (true) {
+                mainHandle();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    void duringTheFight() throws Exception {
+        ATTACK_TIMER.set(0);
+        while (attack.kill()) {
+            count = 0;
+            checkMyHp();
+            SleepTime.sleep(1000);
+            if (ATTACK_TIMER.incrementAndGet() > Prop.ATTACK_TIMER) break;
+        }
+    }
+
+    void findAndKill(KillMonster monster) {
+        try {
+            ATTACK_MOBS_BEHIND_WALLS.set(0);
+            while (monster.kill()) {
+                attackBySwordOrSpell(monster);
+                SleepTime.sleep(2000);
+                count = 0;
+                checkMyHp();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    void attackBySwordOrSpell(KillMonster monster) throws Exception{
+        SleepTime.sleep(2000);
+        killMonstersAround(monster);
+    }
+
+    void killMonstersAround(KillMonster monster) throws Exception {
+        while(monster.findAndKillAround()) {
+//            checkMyHp();
+            SleepTime.sleep(2000);
+            LoggerSingle.logInfo("LogicLocation.killMonstersAround",
+                    "Find monster around, killing");
+            checkMyHp();
+        }
+        actions.heal();
+//        lootAround.takeLootAround();
+    }
+
+    void checkMyHp() throws Exception {
+        actions.pickUpCard();
+        checkHP.checkHp();
+    }
+
+    public abstract void mainHandle() throws Exception;
+    abstract void runFromMonster() throws Exception;
+    abstract void teleport() throws Exception;
+
+
+
+
+
+}
---
+++ src/main/java/logic/LogicWizard/LogicWizardYunField04.java
@@ -0,0 +1,92 @@
+package logic.LogicWizard;
+
+import actions.SleepTime;
+import checks.LocationCheck;
+import checks.location.YunField04;
+import logic.attacks.AttackYun11;
+import logic.hands_rgb.HandYun04;
+import logic.kill_monster.Harpy;
+import logic.take_loot.*;
+import main.Prop;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LogicWizardYunField04 extends LogicLocationWizard {
+
+    private static final int COUNT_OF_ATTACKS = 100;
+
+    public LogicWizardYunField04(int threadId) throws Exception {
+        countOfAttacks = COUNT_OF_ATTACKS;
+        attack = new AttackYun11();
+        locationCheck = new LocationCheck(new YunField04());
+        lootAround.initialize(new HandYun04());
+        killMonsterList = Stream
+                .of(new Harpy())
+                .collect(Collectors.toList());
+
+        loot = new TakeLoot[] {
+//                new BlueHerb(),
+//                new Bottle(),
+////                new AntelopeSkin(),
+                new HarpyFeather(),
+        };
+
+        usefulLoot = new TakeLoot[] {
+                new Card(),
+//                new Shield(),
+                new HarpyFeather(),
+//                new Bottle(),
+//                new BlueHerb(),
+                new Coupon(),
+        };
+
+        checkAgressorIsNear.initialize(Stream
+                .of(new Harpy())
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    public void createThread() throws Exception {
+        start();
+    }
+
+    public void mainHandle() throws Exception {
+        if (checkDie.check()) {
+            while (true) {
+                SleepTime.sleep(5000);
+            }
+        }
+        locationCheck.locationCheck();
+//        checkSP.enoghtSP();
+        killMonsterList.forEach(this::findAndKill);
+        checkMyHp();
+        actions.pickUpCard();
+        actions.pickUpLoot();
+        teleport();
+        count++;
+    }
+
+    //RENAME
+    void checkMyHp() throws Exception {
+        actions.pickUpCard();
+    }
+
+    void teleport() throws Exception {
+        runFromMonster();
+        if (count > Prop.COUNT_TO_TELEPORT) {
+//            lootAround.takeLootAround();
+            sleep(500);
+//            actions.pickUpCard();
+//            actions.pickUpLoot();
+            count = 0;
+            Prop.cast.cast();
+            actions.teleport(locationCheck);
+            actions.stepAside(locationCheck, new int[] {75, 150} );
+        }
+    }
+
+    void runFromMonster() throws Exception {
+    }
+
+}
