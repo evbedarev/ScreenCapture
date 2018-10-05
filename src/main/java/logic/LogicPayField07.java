3d44cdc5ec7c3506f1f6574af30ded3182a280c9
mj <madjo85@yandex.ru>
2018-10-05 11:56:08 +0300

    refactoring +Commit add moving by map + Payon Field 07

--- src/main/java/logic/LogicPayField07.java
+++ src/main/java/logic/LogicPayField07.java
@@ -1,190 +1,96 @@
-//package logic;
-//
-//import checks.*;
-//import checks.location.PayField07;
-//import checks.location.VerifyMap;
-//import email.MsgLocationChanged;
-//import email.SendMessage;
-//import key_and_mouse.Keys;
-//import key_and_mouse.Mouse;
-//import logic.kill_monster.*;
-//import logic.take_loot.*;
-//import org.apache.log4j.LoggerSingle;
-//
-//import java.awt.event.KeyEvent;
-//import java.util.concurrent.atomic.AtomicInteger;
-//
-//public class LogicPayField07 extends Thread implements Logic {
-//    private int count = 0;
-//    private int countForSendMsg = 0;
-//    private final int threadId;
-//    private final Mouse mouse = new Mouse();
-//    private final CheckHP checkHP = new CheckHP(true);
-//    private final static AtomicInteger atomicInt = new AtomicInteger(0);
-//    private LoggerSingle logger = LoggerSingle.getLogger(this.getClass());
-//
-//    VerifyMap verifyMap;
-//    SendMessage sendMessage = new SendMessage();
-//    Keys keys;
-//    Attack attack;
-//    KillMonster killMonster;
-//
-//    private final TakeLoot[] usefulLoot = new TakeLoot[] {
-//            new Card(logger),
-//            new Card1(logger)
-////            new Clothes(),
-////            new Shield(),
-////            new Mask()
-//    };
-//
-//    private final TakeLoot[] loot;
-//
-//
-//    public LogicPayField07(int threadId) throws Exception {
-//        System.out.println(threadId);
-//        verifyMap =  new PayField07();
-//        killMonster = new Bigfoot(logger);
-//
-//        loot = new TakeLoot[] {
-////                new AntelopeHorn(),
-////                new AntelopeSkin(logger),
-////                new BlueHerb(logger),
-//                new Bottle(logger)
-//        };
-//        keys = new Keys();
-//        attack = new Attack(logger);
-//        this.threadId = threadId;
-//    }
-//
-//    public void createThread() throws Exception {
-////        Thread thread = new LogicYunField11(1);
-////        thread.start();
-//        this.start();
-//    }
-//
-//    public void run() {
-//        try {
-//            while (true) {
-//                mainHandle();
-//            }
-//        } catch (Exception exception) {
-//            exception.printStackTrace();
-//        }
-//    }
-//
-//    public void mainHandle() throws Exception {
-//
-//        if (threadId == 0) {
-//            locationCheck();
-//            if (count == 0)
-//                stepAside();
-//            findAndKill();
-//            checkHP.checkHp();
-//            pickUpLoot();
-//            teleport();
-//            count++;
-//            logger.info("Incrase count by 1, count=" + count);
-//        }
-//
-//        if (threadId == 1) {
-//            atomicInt.incrementAndGet();
-//            sleep(1000);
-//        }
-//    }
-//
-//    public void checkMyHp() throws Exception {
-//        checkHP.checkHp();
-//        pickUpCard();
-//    }
-//
-//    void findAndKill() throws Exception{
-//        while (killMonster.kill()) {
-//            count = 0;
-//            logger.info("Set count to " + count);
-//            checkHP.checkHp();
-//            SleepTime.sleep(500);
-//            duringTheFight();
-//        }
-//        if (count == 0)
-//            stepAside();
-//    }
-//
-//    void duringTheFight() throws Exception {
-//        int atk = 1;
-//        while (attack.killOrNot()) {
-//            count = 0;
-//            logger.info("Set count to " + count);
-//            atk++;
-//            checkMyHp();
-//            SleepTime.sleep(500);
-//            if (atk > 20) {
-//                stepAside();
-//                findAndKill();
-//                atk=1;
-//            }
-//        }
-//    }
-//
-//    void pickUpLoot() throws Exception {
-//        pickUpCard();
-//        for (TakeLoot takeLoot: loot) {
-//            takeLoot.pickUp();
-//        }
-//    }
-//
-//    void pickUpCard() throws Exception {
-//        for (TakeLoot takeLoot: usefulLoot) {
-//            takeLoot.pickUp();
-//        }
-//    }
-//
-//
-//    void stepAside() throws Exception {
-//        double t = 2 * Math.PI * Math.random();
-//        double minRadius = 75;
-//        double maxRadius = 150;
-//
-//        double x = minRadius * Math.cos(t);
-//        double x1 = maxRadius * Math.cos(t);
-//
-//        double mediumX = x + Math.random()*(x1 - x);
-//        double mediumR = mediumX/Math.cos(t);
-//        double mediumY = mediumR * Math.sin(t);
-//
-//        mouse.mouseClick(800 + (int) Math.round(mediumX),
-//                450 + (int) Math.round(mediumY));
-//        sleep(1000);
-//    }
-//
-//    void teleport() throws Exception {
-//        if (count > 25) {
-//            logger.info("TELEPORTING count=" + count);
-//            count = 0;
-//            logger.info("Set count to " + count);
-//            keys.keyPress(KeyEvent.VK_F2);
-//            SleepTime.sleep(1000);
-//            keys.keyPress(KeyEvent.VK_ENTER);
-//            SleepTime.sleep(1000);
-//            stepAside();
-//        }
-//    }
-//
-//    void locationCheck() throws Exception {
-//        while (!verifyMap.onDesiredLocation()) {
-//            sleep(5000);
-//            KillMonster goToWarp = new Warp(logger);
-//            logger.info("Нахожусь не на карте!!");
-//            goToWarp.kill();
-//            sleep(2000);
-//            if (verifyMap.onDesiredLocation()) {
-//                teleport();
-//                sleep(2000);
-//            }
-//            countForSendMsg++;
-//            if (countForSendMsg == 100) {
-//                sendMessage.send(new MsgLocationChanged());
-//            }
-//        }
-//        countForSendMsg = 0;
-//    }
-//}
+package logic;
+
+import actions.Actions;
+import checks.LocationCheck;
+import checks.location.GefField05;
+import logic.attacks.AttackGef05;
+import logic.hands_rgb.HandYun11;
+import logic.kill_monster.*;
+import logic.move_by_card.MoveByCard;
+import logic.move_by_card.PointsGefFields05;
+import logic.move_by_card.PointsPayField07;
+import logic.take_loot.Card;
+import logic.take_loot.Coupon;
+import logic.take_loot.TakeLoot;
+import main.Prop;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LogicPayField07 extends LogicLocation {
+
+    private static final int COUNT_OF_ATTACKS = 100;
+    private final static AtomicInteger ATOMIC_GUARD = new AtomicInteger(0);
+    private final static AtomicInteger ATOMIC_AWAKENING = new AtomicInteger(0);
+    private final static AtomicInteger ATOMIC_DEFENDER = new AtomicInteger(0);
+
+
+    public LogicPayField07() throws Exception {
+        countOfAttacks = COUNT_OF_ATTACKS;
+        attack = new AttackGef05();
+        actions = Actions.instance();
+        moveByCard = MoveByCard.getInstance(this);
+        locationCheck = new LocationCheck(new GefField05());
+        lootAround.initialize(new HandYun11());
+        checkHP.initialize(true, Prop.checkHitPoints);
+        killMonsterList = Stream
+                .of(
+                        new Bigfoot(),
+                        new Creamy(),
+                        new Caramel()
+                ).collect(Collectors.toList());
+
+        usefulLoot = new TakeLoot[] {
+                new Card(),
+//            new Clothes(),
+//                new Shield(),
+//            new Mask(logger),
+                new Coupon()
+        };
+
+        loot = new TakeLoot[] {
+//            new PowderOfButterfly(logger),
+        };
+    }
+
+    @Override
+    public void createThread() throws Exception {
+        start();
+    }
+
+    public void mainHandle() throws Exception {
+        moveByCard.move(killMonsterList, new PointsPayField07());
+
+//        locationCheck.locationCheck();
+//        checkSP.enoghtSP();
+//        killMonsterList.forEach(this::findAndKill);
+//        checkMyHp();
+//        actions.pickUpCard();
+//        actions.pickUpLoot(locationCheck);
+//        teleport();
+//        count++;
+//        Prop.cast.cast();
+    }
+
+    public void checkMyHp() throws Exception {
+        actions.pickUpCard();
+        checkHP.checkHp();
+    }
+
+    void teleport() throws Exception {
+        runFromMonster();
+        if (count > Prop.COUNT_TO_TELEPORT) {
+            lootAround.takeLootAround();
+            sleep(500);
+            actions.pickUpCard();
+            actions.pickUpLoot(locationCheck);
+            count = 0;
+            actions.teleport(locationCheck);
+            actions.stepAside(locationCheck, new int[] {75, 150} );
+        }
+    }
+
+    void runFromMonster() throws Exception {
+    }
+}
---
+++ src/main/java/logic/kill_monster/Caramel.java
@@ -0,0 +1,14 @@
+package logic.kill_monster;
+
+import logic.RgbParameter;
+
+import java.awt.*;
+
+public class Caramel extends Monster {
+    public Caramel() throws AWTException {
+        rgbParameterList.add(new RgbParameter(-9745846,
+                new int[] {80,80},
+                new int[] {-1591899, -11390654, -8103334}));
+
+    }
+}
