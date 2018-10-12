f31a336a1ccf8d87a00d1a1d70dd503b199a8693
madjo <madjo85@yandex.ru>
2018-10-12 15:29:41 +0300

    2

--- src/main/java/main/start.java
+++ src/main/java/main/start.java
@@ -1,11 +1,13 @@
 package main;
 
+import actions.SleepTime;
 import logic.*;
 
 public class start {
     static Logic logic;
 
     public static void main(String[] args) throws Exception {
+        SleepTime.sleep(5000);
         Prop.initialize();
         Prop.logic.createThread();
     }
