ab08d2ee75e14d2166c18ddbd0a022fc55cd45a4
madjo <madjo85@yandex.ru>
2018-10-15 01:13:35 +0300

    + yun field 07 + ein dun 01 + gl church + change Loot Around & Loot & LogicLocation

--- src/main/java/getRGB/RGB.java
+++ src/main/java/getRGB/RGB.java
@@ -1,21 +1,32 @@
 package getRGB;
 
+import actions.SleepTime;
+import find_image.FindPixels;
 import logic.Capture;
 import storage_image.StorageImage;
 import storage_image.StorageImageFile;
 
 import java.awt.image.BufferedImage;
+import java.util.Optional;
 
 public class RGB {
     public static void main(String[] args) throws Exception {
+
 //        CompareFragmentImage compareFragmentImage = new CompareFragmentImage();
 //        compareFragmentImage.getRgb();
 //Map rgb -2374501
-        findPixelRgb(-2374501);
+//        findPixelRgb(-2374501);
 
 //        showRGB(new int[] {1480,125}, "C:\\TEMP\\ScreenCapture\\src\\main\\resources\\getRGB\\fragment.png");
 //        showRGB(new int[] {1073,375}, "C:\\TEMP\\ScreenCapture\\src\\main\\resources\\getRGB\\fragment.png");
 //        findPixelRgb(-3219746);
+
+        while (true) {
+            Optional<int[]> xy = takeCoordsFromMap();
+            if (xy.isPresent()) {
+                System.out.println("points.add(new int[] {" + xy.get()[0] +", " + xy.get()[1] +"});");
+            }
+        }
     }
 // Coursor rgb -16250872
 //-3552801/-65794
@@ -52,5 +63,27 @@ public class RGB {
         BufferedImage image = capture.takeScreenShot();
         System.out.println(image.getRGB(xy[0], xy[1]));
 
+    }
+
+    public static Optional<int[]> takeCoordsFromMap() throws Exception {
+        Capture capture = Capture.instance();
+        FindPixels findImageHard = new FindPixels();
+        BufferedImage screenShot;
+        screenShot = capture.takeScreenShot();
+        Optional<int[]> xy = findImageHard.findPixelsInImageInArea(
+                screenShot,
+                -2752512,
+                new int[] {2,2},
+                new int[] {-2752512},
+                new int[] {1459, 1586, 43, 168});
+
+        if (xy.isPresent()) {
+            int x = xy.get()[0];
+            int y = xy.get()[1];
+
+//            LoggerSingle.logInfo(this.toString(), "Location TP " + this.toString() + ", coordinates: x=" + x + " y=" + y);
+            SleepTime.sleep(3000);
+        }
+        return xy;
     }
 }
