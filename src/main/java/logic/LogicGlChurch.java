1f3571b6f52c72f6ff00326ecc99742172da0ad5
mj <madjo85@yandex.ru>
2018-12-13 11:30:42 +0300

    Merge branch 'ranged_change_during_fight' into ranged_change_monster
    # Conflicts:
    #	src/main/java/main/Prop.java

--- src/main/java/logic/LogicGlChurch.java
+++ src/main/java/logic/LogicGlChurch.java
@@ -1,15 +1,12 @@
 package logic;
 
-import actions.SleepTime;
 import checks.LocationCheck;
-import checks.location.EinDun01;
 import checks.location.GlChurch;
 import logic.attacks.AttackYun11;
 import logic.hands_rgb.HandYun04;
 import logic.kill_monster.*;
 import logic.move_by_card.MoveByCard;
 import logic.move_by_card.PointsGlChurch;
-import logic.move_by_card.PointsInSphinx3;
 import logic.take_loot.*;
 import main.Prop;
 
@@ -27,18 +24,20 @@ public class LogicGlChurch extends LogicLocation {
         locationCheck = new LocationCheck(new GlChurch());
         lootAround.initialize(new HandYun04());
         killMonsterList = Stream
-                .of(new EvilDruid(),
+                .of(    new WraithDeath(),
+                        new EvilDruid(),
                         new Wraith(),
                         new Mimic())
                 .collect(Collectors.toList());
 
-        loot = new TakeLoot[] { new Fabric()
+        loot = new TakeLoot[] {
+                new Fabric(),
+                new YggdrasilLeaf(),
+                new WhiteHerb(),
         };
 
         usefulLoot = new TakeLoot[] {
                 new Card(),
-                new YggdrasilLeaf(),
-                new WhiteHerb(),
                 new Coupon(),
         };
 
@@ -56,23 +55,8 @@ public class LogicGlChurch extends LogicLocation {
         moveByCard.move(killMonsterList);
     }
 
-//    public void checkMyHp() throws Exception {
-//        actions.pickUpCard();
-//        checkHP.checkHp();
-//    }
 
     void teleport() throws Exception {
-        runFromMonster();
-        if (count > Prop.COUNT_TO_TELEPORT) {
-            lootAround.takeLootAround();
-            sleep(500);
-            actions.pickUpCard();
-            actions.pickUpLoot(locationCheck);
-            count = 0;
-            Prop.cast.cast();
-            actions.teleport(locationCheck);
-//            actions.stepAside(locationCheck, new int[] {75, 150} );
-        }
     }
 
     void runFromMonster() throws Exception {
