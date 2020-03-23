fc155c344c57f5fd5ea2472025371f76174ba9ed
mj <madjo85@yandex.ru>
2020-03-23 18:34:21 +0300

    change webserver, read last 20 rows in log

--- src/main/java/main/WebServer.java
+++ src/main/java/main/WebServer.java
@@ -9,6 +9,8 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebServer extends Thread{
     @Override
@@ -26,14 +28,39 @@ public class WebServer extends Thread{
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
             byte[] array = Files.readAllBytes(Paths.get("C:\\TEMP\\ScreenCapture\\messages"));
-            t.sendResponseHeaders(200,array.length);
+            byte[] lastTwenty = getLastTwentyRows(array);
+            t.sendResponseHeaders(200,lastTwenty.length);
             OutputStream os = t.getResponseBody();
-            os.write(array);
+            os.write(lastTwenty);
             os.close();
         }
+    }
+
+    private byte[] getLastTwentyRows(byte[] arr) {
+        int countSymbol= 0;
+        byte[] newArray = new byte[arr.length];
+        List<Integer> rows = new ArrayList<>();
+        for (byte b : arr) {
+            if ((char) b == '\n') {
+                rows.add(countSymbol);
+            }
+            countSymbol++;
+        }
+        if (rows.size() > 20) {
+            int j = 0;
+            for (int i = rows.get(rows.size() - 20) ; i < arr.length; i++) {
+                newArray[j++] = arr[i];
+            }
+            byte[] finalArr = new byte[j];
+            System.arraycopy(newArray, 0, finalArr, 0, j);
+            return finalArr;
+        } else {
+            newArray = arr;
+        }
+        return newArray;
     }
 }
